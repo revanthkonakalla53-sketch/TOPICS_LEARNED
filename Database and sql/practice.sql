fc420a4ae09986c9d1c78df8b74d201e19cf8bf4
revanth <revanthkonakalla53@gmail.com>
2026-09-22 21:37:42 +0530

    code 3

--- Database and sql/practice.sql
+++ Database and sql/practice.sql
@@ -11,3 +11,10 @@ CREATE TABLE customer (
 
 
 
+CREATE TABLE order_details(
+  order_id integer,
+  customer_id integer,
+  order_datetime datetime,
+  shipped_datetime datetime,
+  total_amount float
+);
