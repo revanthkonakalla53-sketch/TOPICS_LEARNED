2c8b79b78815d77bf197e849e94bb273009bd956
revanth <revanthkonakalla53@gmail.com>
2026-09-22 22:11:34 +0530

    code 7

--- Database and sql/practice.sql
+++ Database and sql/practice.sql
@@ -40,3 +40,13 @@ INSERT INTO
   player(name, age, score)
 VALUES
   ("Raj", 26, 120)  
+
+
+
+
+  INSERT INTO
+  player(name, age, score)
+VALUES
+  ("Ram", 28, 125),
+  ("Charan", 25, 173),
+  ("Ravan", 20, 152);
